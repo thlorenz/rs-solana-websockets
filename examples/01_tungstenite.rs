66d3d7d6960069ffdfbcb583344860ad27c2dc3f
Thorsten Lorenz <thlorenz@gmx.de>
2024-05-11 15:57:08 -0500

    feat: echo server via tokio-tungstenite

--- examples/01_tungstenite.rs
+++ examples/01_tungstenite.rs
@@ -11,7 +11,6 @@ fn main() {
             loop {
                 let msg = websocket.read().unwrap();
 
-                // We do not want to send back ping/pong messages.
                 if msg.is_binary() || msg.is_text() {
                     websocket.send(msg).unwrap();
                 }
